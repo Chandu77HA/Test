f9d7e2d7aa684241e5afe34611e28f35587ffdb4
Chandu77HA <hachandan02@gmail.com>
2024-04-18 11:37:51 +0530

    User History Modified

--- doc1/views.py
+++ doc1/views.py
@@ -6,7 +6,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from llama_index.embeddings.langchain import LangchainEmbedding
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import ChatPromptTemplate
-from llama_index.core.llms import ChatMessage,MessageRole
+from llama_index.core.llms import ChatMessage, MessageRole
 from django.conf import settings
 from llama_index.llms.together import TogetherLLM
 import os
@@ -23,19 +23,17 @@ from llama_index.core import load_index_from_storage
 from llama_index.core import StorageContext
 
 
-
-
-
 def home(request):
     return render(request, 'index.html')
 
 # def doc_view(request):
 #     return render(request, 'doc.html')
 
+
 def doc_view(request):
 
-
     return render(request, 'doc.html')
+
 
 def split_pdf_by_page(pdf_file, output_folder):
     # Delete the output folder if it exists
@@ -52,7 +50,8 @@ def split_pdf_by_page(pdf_file, output_folder):
     for page_num in range(num_pages):
         pdf_writer = PyPDF2.PdfWriter()
         pdf_writer.add_page(pdf_reader.pages[page_num])
-        output_file_path = os.path.join(output_folder, f'page_{page_num + 1}.pdf')
+        output_file_path = os.path.join(
+            output_folder, f'page_{page_num + 1}.pdf')
         pdf_names.append(f'page_{page_num + 1}')
         with open(output_file_path, 'wb') as output_pdf:
             pdf_writer.write(output_pdf)
@@ -62,7 +61,8 @@ def split_pdf_by_page(pdf_file, output_folder):
 
 def build_document_summary_index(city_docs):
     # Initialize the embedding model
-    embed_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name=settings.SENTENCE_EMBEDDING_MODEL))
+    embed_model = LangchainEmbedding(HuggingFaceEmbeddings(
+        model_name=settings.SENTENCE_EMBEDDING_MODEL))
     settings.embed_model = embed_model
 
     # Initialize the LLM model
@@ -76,7 +76,8 @@ def build_document_summary_index(city_docs):
     splitter = SentenceSplitter(chunk_size=settings.SPLITTER_CHUNK_SIZE)
 
     # Initialize the response synthesizer
-    response_synthesizer = get_response_synthesizer(response_mode="tree_summarize", use_async=True)
+    response_synthesizer = get_response_synthesizer(
+        response_mode="tree_summarize", use_async=True)
 
     # Build the document summary index
     doc_summary_index = DocumentSummaryIndex.from_documents(
@@ -90,12 +91,9 @@ def build_document_summary_index(city_docs):
     return doc_summary_index
 
 
-
-
-
-
 doc_summary_index = None
 city_docs = []
+
 
 def formatted_answer(answer):
     lines = answer.split('\n')
@@ -114,16 +112,19 @@ def formatted_answer(answer):
         if numbered_match:
             if not in_list or list_type != 'ol':
                 if in_list:  # Close the previous list
-                    formatted_lines.append('</ul>' if list_type == 'ul' else '</ol>')
+                    formatted_lines.append(
+                        '</ul>' if list_type == 'ul' else '</ol>')
                 formatted_lines.append('<ol>')
                 in_list = True
                 list_type = 'ol'
-            formatted_lines.append(f'<li>{numbered_match.group(2).strip()}</li>')
+            formatted_lines.append(
+                f'<li>{numbered_match.group(2).strip()}</li>')
 
         elif asterisk_match or asterisk_items:
             if not in_list or list_type != 'ul':
                 if in_list:  # Close the previous list
-                    formatted_lines.append('</ol>' if list_type == 'ol' else '</ul>')
+                    formatted_lines.append(
+                        '</ol>' if list_type == 'ol' else '</ul>')
                 formatted_lines.append('<ul>')
                 in_list = True
                 list_type = 'ul'
@@ -131,11 +132,13 @@ def formatted_answer(answer):
                 for item, _ in asterisk_items:
                     formatted_lines.append(f'<li>{item.strip()}</li>')
             else:
-                formatted_lines.append(f'<li>{asterisk_match.group(2).strip()}</li>')
+                formatted_lines.append(
+                    f'<li>{asterisk_match.group(2).strip()}</li>')
 
         else:
             if in_list:  # Close the previous list
-                formatted_lines.append('</ul>' if list_type == 'ul' else '</ol>')
+                formatted_lines.append(
+                    '</ul>' if list_type == 'ul' else '</ol>')
                 in_list = False
             # Wrap non-list lines in paragraphs or handle them appropriately
             formatted_lines.append(f'<p>{line.strip()}</p>')
@@ -149,7 +152,11 @@ def formatted_answer(answer):
 
     return formatted_output
 
-query_history=[]
+
+query_history = []
+
+""" 
+
 def process_file(request):
     global doc_summary_index
     global city_docs
@@ -213,7 +220,7 @@ def process_file(request):
                    ]
 
                    text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
-                   query_2 = f"""{user_input} """
+                   query_2 = f'''{user_input} '''
                    query_engine = RetrieverQueryEngine(
                        retriever=retriever,
                        response_synthesizer=response_synthesizer
@@ -228,9 +235,86 @@ def process_file(request):
     # Render a response for the GET request
     return render(request, 'doc_chat.html')
 
+"""
+
+
+def process_file(request):
+    global doc_summary_index
+    global city_docs
+    global query_history
+
+    if request.method == 'POST':
+        if request.FILES.get('pdf_file'):
+            # Handle PDF file upload
+            pdf_file = request.FILES['pdf_file']
+            output_folder = 'output_folder'
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+            city_docs = []
+            output_folder = 'output_folder'
+            pdf_names = split_pdf_by_page(pdf_file, output_folder)
+            for wiki_title in pdf_names:
+                docs = SimpleDirectoryReader(
+                    input_files=[f"{output_folder}/{wiki_title}.pdf"]
+                ).load_data()
+                for doc in docs:
+                    doc.doc_id = wiki_title
+                city_docs.extend(docs)
+            doc_summary_index = build_document_summary_index(city_docs)
+            doc_summary_index.storage_context.persist("index")
+            # Clear query history when a new file is uploaded
+            query_history.clear()
+            return render(request, 'doc_chat.html')
+
+        elif request.POST.get('user_input'):
+            # Handle user input
+            user_input = request.POST.get('user_input', '')
+            if user_input:
+                try:
+                    chat_text_qa_msgs = [
+                        ChatMessage(
+                            role=MessageRole.SYSTEM,
+                            content=(
+                                "Summarize the documents.\n"
+                                "Always answer the query using the provided context information, "
+                            ),
+                        ),
+                        ChatMessage(
+                            role=MessageRole.USER,
+                            content=(
+                                "Context information is below.\n"
+                                "---------------------\n"
+                                "{context_str}\n"
+                                "---------------------\n"
+                                # "Given the context information, Provide a summary to the document .\n"
+                                " Please write a passage to answer the question\n"
+                                "Try to include as many key details as possible.\n"
+
+                            ),
+                        ),
+                    ]
+
+                    text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
+                    query_2 = f"""{user_input} """
+                    # query_engine = RetrieverQueryEngine(
+                    #     retriever=retriever,
+                    #     response_synthesizer=response_synthesizer
+                    # )
+                    response__2 = doc_summary_index.as_query_engine(
+                        text_qa_template=text_qa_template).query(query_2)
+                    answer = formatted_answer(str(response__2))
+                    query_history.append(
+                        {"question": user_input, "answer": answer})
+                    return render(request, 'doc_chat.html', {'query_history': query_history})
+
+                except AttributeError:
+                    # Handle the case when doc_summary_index is None
+                    return HttpResponseServerError("Something went wrong. Please retry uploading the PDF.")
+        # Render a response for the GET request
+    return render(request, 'doc_chat.html', {'query_history': query_history})
+
+
 def doc_chat(request):
     # return render(request, 'doc_chat.html')
 
     return render(request, 'doc_chat.html')
-
-
